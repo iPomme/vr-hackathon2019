b4df099f13fe0c22624bc982770e825190823a94
Olivier Amrein <oenvoyage@gmail.com>
2019-11-11 11:07:09 +0100

    PiggyVR : WebGL test Piggy and goldcoins

---
+++ TimeChair/Assets/piggyVR/scripts/cannon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour
+{
+    public Rigidbody goldcoin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            Rigidbody newcoin;
+            newcoin = Instantiate(goldcoin, transform.position, transform.rotation);
+            newcoin.velocity = transform.TransformDirection(Vector3.forward * 10);
+
+        }
+    }
+    
+}
