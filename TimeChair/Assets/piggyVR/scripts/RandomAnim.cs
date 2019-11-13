747e88c2658239795ef46855fa4329d34316b5fa
Nicolas Jorand <nicolas.jorand@me.com>
2019-11-13 19:59:40 +0100

    Add random script

---
+++ TimeChair/Assets/piggyVR/scripts/RandomAnim.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomAnim : MonoBehaviour
+{
+
+    private Rigidbody rigid;
+    
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the game object that this script is attached to by 15 in the X axis,
+        // 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
+        // rather than per frame.
+        transform.Rotate (new Vector3 (0, Random.Range(-45,45), 0) * Time.deltaTime);
+
+//        rigid.velocity = transform.TransformDirection(Vector3.forward*2f);
+
+
+    }
+}
