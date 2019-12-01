dc6810066f8e99be6c9df28fc1035f2cfabc1a4a
Nicolas Jorand <nicolas.jorand@me.com>
2019-12-01 01:16:38 +0100

    Add the chair management

---
+++ TimeChair/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Color;
+
+public class ColorChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool isPast;
+    
+    private Color originalColor;
+
+    private void Start()
+    {
+        originalColor = transform.GetComponent<Renderer>().material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigered");
+        transform.GetComponent<Renderer>().material.SetColor("_Color", gray);
+        ChairTracker chair = other.GetComponent<ChairTracker>();
+        if (isPast)
+        {
+            chair.enterPast();
+        }
+        else
+        {
+            chair.enterFuture();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.GetComponent<Renderer>().material.SetColor("_Color", originalColor);
+        ChairTracker chair = other.GetComponent<ChairTracker>();
+        if (isPast)
+        {
+            chair.exitPast();
+        }
+        else
+        {
+            chair.exitFuture();
+        }
+    }
+}
