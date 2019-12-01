1a5652c781f0ba9830f6e906b31cf6ed397f82c4
Nicolas Jorand <nicolas.jorand@me.com>
2019-12-01 03:01:19 +0100

    set trigger for animation

--- TimeChair/Assets/Scripts/ChairTracker.cs
+++ TimeChair/Assets/Scripts/ChairTracker.cs
@@ -10,24 +10,35 @@ public class ChairTracker : MonoBehaviour
 
     private float initialHeight;
 
-    private float deltaUp = 20f;
+    private float deltaUp = .20f;
 
-    private float deltaBack = 5f;
+    private float deltaBack = .05f;
+
+    private bool chairDown = true;
 
     // Update is called once per frame
     void Update()
     {
         if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
         {
+            Debug.LogFormat("Chair Setup at '{0}'", transform.position.y);
+
             initialHeight = chair.transform.position.y;
+            chairDown = true;
         }
 
-        if (initialHeight != null && initialHeight > initialHeight + deltaUp)
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.LogFormat("Current Position '{0}'", transform.position);
+        }
+
+        if (initialHeight != null && transform.position.y > initialHeight + deltaUp && chairDown)
         {
             Debug.Log("Chair UP");
+            chairDown = false;
         }
 
-        if (initialHeight != null && initialHeight < initialHeight + deltaBack)
+        if (initialHeight != null && transform.position.y < initialHeight + deltaBack && !chairDown)
         {
             Debug.Log("Chair Down");
         }
